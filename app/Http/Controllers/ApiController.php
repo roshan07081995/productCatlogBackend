21fc4bf61e08901e26c8e92c0f3b8bc93c03554e
Roshan <roshanshetkar@gmail.com>
2021-01-28 18:04:32 +0530

    code cleaning

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -17,66 +17,23 @@ class ApiController extends Controller
     }
 
     public function createProduct(Request $request) {
-      $product = new Product;
-      $product->pName = $request->pName;
-      $product->pDesc = $request->pDesc;
-      $product->pQuantity = $request->pQuantity;
-      $product->pAmount = $request->pAmount;
-	
-	
-	$image = $request->fileName;  // your base64 encoded
-    $image = str_replace('data:image/png;base64,', '', $image);
-    $image = str_replace(' ', '+', $image);
-    //$imageName =str_random(10).'.'.'png';
-	$imageName = Str::random(10).'.'.'png';
-    Storage::disk('local')->put($imageName, base64_decode($image));
-	
-    // $response = array(
-        // 'status' => 'success',
-    // );
-    // return Response::json( $response  );
-	
-	
-      // if(!$request->hasFile('fileName')) {
-        // return response()->json(['upload_file_not_found'], 400);
-    // }
-	//$path = $this->createImage($request->fileName);
-    // $allowedfileExtension=['pdf','jpg','png'];
-    // $file = $request->file('fileName'); 
-	// $extension = $file->getClientOriginalExtension();
-	// $check = in_array($extension,$allowedfileExtension);
-	// if($check) {
-      	// $uploadFolder = 'users';
-		// $image = $request->file('fileName');
-		// $image_uploaded_path = $image->store($uploadFolder, 'public');
-        // }
-		// else {
-            // return response()->json(['invalid_file_format'], 422);
-        // }
-
+		$product = new Product;
+		$product->pName = $request->pName;
+		$product->pDesc = $request->pDesc;
+		$product->pQuantity = $request->pQuantity;
+		$product->pAmount = $request->pAmount;
+		$image = $request->fileName;  // your base64 encoded
+		$image = str_replace('data:image/png;base64,', '', $image);
+		$image = str_replace(' ', '+', $image);
+		$imageName = Str::random(10).'.'.'png';
+		Storage::disk('local')->put($imageName, base64_decode($image));
 		$product->pImage = $imageName;
-        $product->save();
-		  return response()->json([
-			"message" => "product record created"
-		  ], 201);
+		$product->save();
+		return response()->json([
+		"message" => "product record created"
+		], 201);
     }
 	
-	
-	 public function createImage($img)
-    {
-	
-		
-		// $folderPath = "users";
-
-		// $image_parts = explode(";base64,", $img);
-		// $image_type_aux = explode("image/", $image_parts[0]);
-		// $image_type = $image_type_aux[1];
-		// $image_base64 = base64_decode($image_parts[1]);
-		// $file = $folderPath . uniqid() . '. '.$image_type;
-
-		// file_put_contents($file, $image_base64);
-		// return $file;
-    }
 	
     public function getProduct($id) {
       if (Product::where('id', $id)->exists()) {
